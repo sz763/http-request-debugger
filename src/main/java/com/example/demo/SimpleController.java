ee46117fa14b99edae1e148f95a22b77ff66a789
Salavat_Zainullin <Salavat_Zainullin@epam.com>
2021-05-25 11:46:16 +0400

    init

---
+++ src/main/java/com/example/demo/SimpleController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/test")
+public class SimpleController {
+
+    @RequestMapping
+    //Http method didn't specified to receive all requests
+    public ResponseEntity<String> handleRequest(HttpServletRequest request) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        appendHeaders(request, builder);
+        appendMultiParts(request, builder);
+        appendParameters(request, builder);
+        return ResponseEntity.ok(builder.toString());
+    }
+
+    private void appendParameters(HttpServletRequest request, StringBuilder builder) {
+        Map<String, String[]> map = request.getParameterMap();
+        map.entrySet().stream().map(e -> e.getKey() + '=' + Arrays.toString(e.getValue()))
+                .forEach(e -> builder.append(e).append('\n'));
+    }
+
+    private void appendHeaders(HttpServletRequest request, StringBuilder builder) {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        builder.append("===========headers===============\n");
+        headerNames.asIterator()
+                .forEachRemaining(headerName ->
+                        builder.append(headerName).append('=').append(request.getHeader(headerName)).append('\n')
+                );
+        builder.append("===========headers============\n");
+    }
+
+    private void appendMultiParts(HttpServletRequest request, StringBuilder builder) throws IOException {
+        builder.append("===========multi parts============\n");
+        try {
+            request.getParts().forEach(part -> builder
+                    .append(part.getName()).append(',').append(part.getContentType()).append('\n'));
+        } catch (ServletException e) {
+            builder.append("can't get file parts\n");
+        }
+        builder.append("===========multi parts============\n");
+    }
+}
